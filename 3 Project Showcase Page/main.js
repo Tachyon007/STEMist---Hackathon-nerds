a08813ec9e943a0bce21ccdec3b156c812390e2d
Tachyon <kev.jelinek@gmail.com>
2023-07-23 03:17:24 -0400

    tried doing image stuff

--- 3 Project Showcase Page/main.js
+++ 3 Project Showcase Page/main.js
@@ -38,22 +38,23 @@ function handleProjectSubmission(){
         console.log(fName, fProjName, projPrompt, fDesc,)
         console.log("img --> char length : " + reader.result.length);
     
-      /*fetch('https://ideaspark-hack.glitch.me/addPersonalProject', {
+      fetch('https://ideaspark-hack.glitch.me/addPersonalProject', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          authorName: fName,
-          projName: fProjName,
-          description: fDesc,
+          fName: fName,
+          fProjName: fProjName,
+          fDesc: fDesc,
+          projPrompt: projPrompt,
 
-          images: JSON.stringify(reader.result),
+          fImg: JSON.stringify(reader.result),
         })
       }).then((res) => {
         console.log(res);
-      })*/
+      })
     
     }
 
@@ -62,3 +63,77 @@ function handleProjectSubmission(){
 }
 
 //load relevant pages
+
+function renderloadedContent(objArr){
+    let container = document.getElementById("project-container");
+
+    while(container.children.length > 0){
+        container.removeChild(container.firstChild);
+    }
+
+    for(let i = 0; i < objArr.length; i++){
+
+        let m = document.createElement("div");
+        m.className = "mainbox";
+
+        let d1 = document.createElement("div");
+        d1.id = "titlebox";
+        d1.innerText = objArr[i].Title;
+
+        let d2 = document.createElement("div");
+        d2.id = "descbox";
+        d2.innerHTML = objArr[i].Description;
+
+        let d3 = document.createElement("div");
+        d3.id = "infobox";
+        d3.innerHTML = objArr[i].author;
+
+        let d4 = document.createElement("div");
+        d4.id = "imagebox";
+       /* let image = document.createElement("img");
+        d4.appendChild(image);
+        image.src = objArr[i].image;
+        image.alt = "";
+
+        image.onload = () =>{
+            image.src = objArr[i].image;
+            alert(1)
+        }*/
+        
+
+
+        console.log(objArr[i].image)
+
+        let d5 = document.createElement("div");
+        d5.id = "ratingbox";
+        d5.innerHTML = "";
+
+        m.appendChild(d1);
+        m.appendChild(d2);
+        m.appendChild(d3);
+        m.appendChild(d4);
+        m.appendChild(d5);
+
+        container.appendChild(m);
+
+    }
+
+}
+
+function loadProjects(){
+    fetch('https://ideaspark-hack.glitch.me/lookUpProject', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          Prompt:currentProjTitle,
+        })
+      }).then((res) => {
+            res.json().then((loadedResponse)=>{
+                console.log(loadedResponse);
+                renderloadedContent(loadedResponse);
+            })
+      })
+}
